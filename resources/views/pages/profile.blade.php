14a55080ddd4da3dc89bbe20d34afe842f39ed75
janhavi <jg688@njit.edu>
2018-11-15 22:05:15 -0500

    Feature: Created profile page, controller, route and added link for it to navigation

---
+++ resources/views/pages/profile.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Profile</div>
+
+                    <div class="card-body">
+                        <div class="row"> <label class="col-2 font-weight-bold">First Name: </label> <span class="col-10">{{$profile->first_name}}</span></div>
+                        <div class="row"><label class="col-2 font-weight-bold">Last Name: </label> <span class="col-10">{{$profile->last_name}}</span></div>
+                        <div class="row"><label class="col-2 font-weight-bold">About: </label> <span class="col-10">{{$profile->body}}</span></div>
+                    </div>
+
+                    <div class="card-footer">
+                        <a class="btn btn-success float-right" href="#">
+                            Edit
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
