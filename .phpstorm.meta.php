c49cc0f8174f256d6e7d648896bcf707b0e36396
janhavi <jg688@njit.edu>
2018-12-15 02:13:49 -0500

    Task: testing setup for laravel dusk

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-11-16 22:59:23.
+    * Generated on 2018-12-15 06:48:01.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.13 on 2018-11-16 22:59:22.
+ * Generated for Laravel 5.7.13 on 2018-12-15 06:48:00.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
