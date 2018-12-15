fa416ebacba1b2c2ed2d484d58c7f12b2b8008b1
janhavi <jg688@njit.edu>
2018-12-15 18:46:06 -0500

    Fix: order of test functions updated + minor cosmetic changes

--- tests/Browser/LoginAuthenticationTest.php
+++ tests/Browser/LoginAuthenticationTest.php
@@ -10,10 +10,42 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class LoginAuthenticationTest extends DuskTestCase
 {
+    public function testUserVerification(){
+
+
+        $this->browse(function ($browser)  {
+            $browser->visit('/register')
+                ->type('email', 'testUserVerify@test.com')
+                ->type('password', 'secret')
+                ->type('password_confirmation', 'secret')
+                ->press('Register');
+
+        });
+
+        $user= User::where('email','testUserVerify@test.com')->first();
+
+        $this->browse(function ($browser)  use($user){
+            $userToken= $user->user_activation_token;
+            $browser->visit(url('user/verify', $userToken))
+                ->assertPathIs('/login');
+        });
+
+        $this->browse(function ($browser) use ($user) {
+            $browser->visit('/login')
+                ->type('email', $user->email)
+                ->type('password', 'secret')
+                ->press('Login')
+                ->assertPathIs('/home');
+        });
+
+
+        $user->delete();
+    }
+
     public function testUserLogin()
     {
         $user = factory(User::class)->make([
-            'email' => 'janhavi@abc.com',
+            'email' => 'testLogin@test.com',
         ]);
         $user->save();
         $this->browse(function ($browser) use ($user) {
@@ -24,31 +56,9 @@ class LoginAuthenticationTest extends DuskTestCase
                 ->assertPathIs('/home');
         });
 
-       $user->delete();
-    }
-
-    public function testUserVerification(){
-
-
-        $this->browse(function ($browser)  {
-            $browser->visit('/register')
-                ->type('email', 'gk.janvi009@gmail.com')
-                ->type('password', 'secret')
-                ->type('password_confirmation', 'secret')
-                ->press('Register');
-
-        });
-
-        $user= User::where('email','gk.janvi009@gmail.com')->first();
-
-        $this->browse(function ($browser)  use($user){
-            $userToken= $user->user_activation_token;
-            $browser->visit(url('user/verify', $userToken))
-                ->assertPathIs('/login');
-        });
-
         $user->delete();
     }
+
 
 
 }
