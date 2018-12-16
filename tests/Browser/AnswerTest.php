aedb727d4a24c348b7f58b0094eb8926fccf8206
janhavi <jg688@njit.edu>
2018-12-15 19:02:24 -0500

    Feature : Added dusk test to edit answer

--- tests/Browser/AnswerTest.php
+++ tests/Browser/AnswerTest.php
@@ -39,4 +39,37 @@ class AnswerTest extends DuskTestCase
         $question->delete();
         $user->delete();
     }
+
+    public function testEditAnswerToQuestion(){
+        $user = factory(User::class)->make([
+            'email' => 'testEditAnswer@abc.com',
+        ]);
+        $user->save();
+        $this->browse(function ($browser) use ($user) {
+            $browser->visit('/login')
+                ->type('email', $user->email)
+                ->type('password', 'secret')
+                ->press('Login')
+                ->assertPathIs('/home')
+                ->clickLink('Post New')
+                ->assertPathIs('/question/create')
+                ->type('body', 'Question to be answered')
+                ->press('Post')
+                ->assertPathIs('/home')
+                ->clickLink('View')
+                ->clickLink('Post New')
+                ->type('body', 'Answer to be updated')
+                ->press('Post')
+                ->clickLink('View')
+                ->clickLink('Edit')
+                ->type('body', 'Answer updated')
+                ->press('Post')
+                ->assertSee("Answer successfully updated !!");
+        });
+
+        $question = Question::where('user_id',($user->id))->first();
+        Answer::where('question_id',($question->id))->delete();
+        $question->delete();
+        $user->delete();
+    }
 }
