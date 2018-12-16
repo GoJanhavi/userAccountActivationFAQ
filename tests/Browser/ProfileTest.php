68d0e8f9df3b4cefc474534df68e727032d22ce6
janhavi <jg688@njit.edu>
2018-12-15 19:07:27 -0500

    Feature : Added dusk test for updating / adding profile details

--- tests/Browser/ProfileTest.php
+++ tests/Browser/ProfileTest.php
@@ -2,22 +2,34 @@
 
 namespace Tests\Browser;
 
+use App\User;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ProfileTest extends DuskTestCase
 {
-    /**
-     * A Dusk test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                    ->assertSee('Laravel');
+    public function testCreateProfile(){
+        $user = factory(User::class)->make([
+            'email' => 'testAddProfile@test.com',
+        ]);
+        $user->save();
+        $this->browse(function ($browser) use ($user) {
+            $browser->visit('/login')
+                ->type('email', $user->email)
+                ->type('password', 'secret')
+                ->press('Login')
+                ->assertPathIs('/home')
+                ->clickLink('My Account')
+                ->clickLink('Create Profile')
+                ->type('first_name','TestFirstName')
+                ->type('last_name','TestLastName')
+                ->type('body','Test content for About Me')
+                ->press('Save')
+                ->assertPathIs('/home');
         });
+
+        $user->delete();
     }
+
 }
